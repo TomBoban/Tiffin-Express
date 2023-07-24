dc0ed6eeacb41e1ad9a6257a6f74b0733fa5f31c
Shradhdha-10 <shradhdhaparekh99@gmail.com>
2023-07-24 15:08:43 -0400

    landing page changes

--- frontend/tiffin-service/src/pages/LandingPage/LandingPage.js
+++ frontend/tiffin-service/src/pages/LandingPage/LandingPage.js
@@ -5,6 +5,7 @@ import "./LandingPage.css";
 import { motion } from "framer-motion";
 import { Link, useHistory } from "react-router-dom";
 import { EyeOutlined } from "@ant-design/icons";
+import {FaUtensils} from "react-icons/fa";
 
 import AnimateButton from "../../components/Buttons/AnimateButton";
 
@@ -16,7 +17,8 @@ const LandingPage = () => {
   };
 
   return (
-    <Container
+    <>
+     <Container
       maxWidth={false}
       sx={{
         minHeight: "100vh",
@@ -172,7 +174,56 @@ const LandingPage = () => {
           sx={{ display: { xs: "none", md: "flex" } }}
         ></Grid>
       </Grid>
+      <Grid>
+      
+      </Grid>
     </Container>
+    <main id="main">
+
+
+<section id="about" className="about">
+  <div className="container-fluid">
+
+    <div className="row">
+
+      <div className="col-lg-5 align-items-stretch video-box">
+        <img src="/images/foodImage.jpg" width="633px" height="470px" alt="image1"/>
+      </div>
+
+      <div className="col-lg-7 d-flex flex-column justify-content-center align-items-stretch">
+
+        <div className="content">
+          <h3>Eum ipsam laborum deleniti <strong>velit pariatur architecto aut nihil</strong></h3>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Duis aute irure dolor in reprehenderit
+          </p>
+          <p className="fst-italic">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
+            magna aliqua.
+          </p>
+          <ul>
+            <li><i><FaUtensils /></i> Ullamco laboris nisi ut aliquip ex ea commodo consequat.</li>
+            <li><i><FaUtensils /></i> Duis aute irure dolor in reprehenderit in voluptate velit.</li>
+            <li><i><FaUtensils /></i> Ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate trideta storacalaperda mastiro dolore eu fugiat nulla pariatur.</li>
+          </ul>
+          <p>
+            Ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+            velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum
+          </p>
+        </div>
+
+      </div>
+
+    </div>
+
+  </div>
+</section>
+</main>
+    </>
+   
+    
+     
   );
 };
 
--- frontend/tiffin-service/src/pages/ProductDetails/ProductDetails.js
+++ frontend/tiffin-service/src/pages/ProductDetails/ProductDetails.js
@@ -4,6 +4,8 @@ import { CircularProgress, Grid, Paper,CardMedia } from "@mui/material";
 import { useDispatch, useSelector } from "react-redux";
 import { getSingleProduct } from "../../redux/slice/productsSlice";
 import { Link, useParams, useHistory } from "react-router-dom";
+import { AiFillStar, AiOutlineStar } from 'react-icons/ai';
+import {FaUtensils} from "react-icons/fa6";
 
 export const ProductDetails = () => {
   const dispatch = useDispatch();
@@ -33,12 +35,72 @@ export const ProductDetails = () => {
           <CircularProgress />
         ) : (
           <>
-            <Grid item lg={6} sm={12}>
-          <CardMedia  src={prodList?.image}/>
-            </Grid>
-            <Grid item lg={6} sm={12}>
-              Details
-            </Grid>
+           
+
+    <div>
+      <div className="product-detail-container">
+        <div>
+          <div className="image-container">
+            <img src={prodList?.image} alt="" className="product-detail-image" />
+          </div>
+          <div className="small-images-container">
+            {/* {image?.map((item, i) => (
+              <img 
+                key={i}
+                src={urlFor(item)}
+                className={i === index ? 'small-image selected-image' : 'small-image'}
+                onMouseEnter={() => setIndex(i)}
+              />
+            ))} */}
+          </div>
+        </div>
+
+        <div className="product-detail-desc">
+          <h1>{prodList?.name}</h1>
+          <div className="reviews">
+            <div>
+              <AiFillStar />
+              <AiFillStar />
+              <AiFillStar />
+              <AiFillStar />
+              <AiOutlineStar />
+            </div>
+            <p>
+              (20)
+            </p>
+          </div>
+          <h4>Description: </h4>
+          <p>{prodList?.description}</p>
+          <p className="price">Price: ${prodList?.price}</p>
+          <h4>Menu: </h4>
+          <p className="menuOpt"><span className="menu-icon"><FaUtensils /></span><b>Menu 1: </b>{prodList?.menuOption1}</p>
+          <p className="menuOpt"><span className="menu-icon"><FaUtensils /></span><b>Menu 2: </b>{prodList?.menuOption2}</p>
+          <p className="menuOpt"><span className="menu-icon"><FaUtensils /></span><b>Menu 3: </b>{prodList?.menuOption3}</p>
+          {/* <div className="quantity">
+            <h3>Quantity:</h3>
+            <p className="quantity-desc">
+              <span className="minus" onClick={decQty}><AiOutlineMinus /></span>
+              <span className="num">{qty}</span>
+              <span className="plus" onClick={incQty}><AiOutlinePlus /></span>
+            </p>
+          </div> */}
+          <div className="buttons">
+            <button type="button" className="add-to-cart" >Subscribe</button>
+          </div>
+        </div>
+      </div>
+
+      {/* <div className="maylike-products-wrapper">
+          <h2>You may also like</h2>
+          <div className="marquee">
+            <div className="maylike-products-container track">
+              {products.map((item) => (
+                <Product key={item._id} product={item} />
+              ))}
+            </div>
+          </div>
+      </div> */}
+    </div>
           </>
         )}
       </Grid>
